0a2dba7391c52211415335168d6ec0308ee5d122
Mr.Grocha <97038811+AbdoPy@users.noreply.github.com>
2023-09-17 22:55:49 +0100

    Update sympy.py

--- sympy.py
+++ sympy.py
@@ -10,4 +10,3 @@ n, l = smp.symbols('n l', integer=True, positive=True)
 
 R = smp.sqrt((2/(n*a))**3*smp.factorial(n-l-1)/(2*n*(smp.factorial(n+1))))*smp.exp(-r/(n*a))*(2*r/(n*a))**l*assoc_laguerre(n-l-1, 2*l+1, (2*r/(n*a)))
 
-print(R)
